cf041cb7f91c2afc8849b603575c4e99d30d4aa9
bobabarbanel <bob.abarbanel@gmail.com>
2017-10-27 12:58:27 -0700

    Cleanup

--- images.js
+++
@@ -1,8 +0,0 @@
-var db = connect("localhost:27017/parts");
-var cursor = db.images.aggregate([{ $match: {} }, { $project: { 
-    "files.filename": 1, "files.dir": 1, _id: 0 } },
-{ $unwind: { path: "$files" } }]);
-
-while (cursor.hasNext()) {
-    printjson(cursor.next());
-}
--- public/util/dirs2.js
+++
@@ -1,7 +0,0 @@
-// dirs2.js
-const dirTree = require("directory-tree");
-const path = require('path');
-const tree = dirTree("C:/Users/RAbarbanel/Documents/mnode/public");/*, null,
-	//(item, path) => console.log(item));*/
-const util = require('util');
-console.dir(tree, {depth: null, colors: true});
